930509dc366ee643db38cb0197364bb606687a06
Cristina <tu-emailejemplo.com>
2026-05-04 14:49:00 +0200

    feat: add dojo architecture to tools section (wainscoting and  tatami)

--- src/pages/Tools.jsx
+++ src/pages/Tools.jsx
@@ -4,25 +4,37 @@ import skillsData from "../services/skillsData.json";
 function Tools() {
   return (
     <section className="tools">
-      <h2 className="tools__title">Mi tools</h2>
-      <ul className="tools__list">
-        {skillsData.map((skill, index) => (
-          <li
-            key={skill.id}
-            className={`tools__item tools__item--${skill.color}`}
-            style={{ animationDelay: `${index * 0.2}s` }}
-          >
-            {/* El cordel del cuadro */}
-            <span className="tools__hook"></span>
+      {/* --- ELEMENTOS DE FONDO --- */}
+      <div className="tools__ceiling"></div>
+      <div className="dojo__side-structure"></div>
 
-            {/* Contenedor cuadro */}
-            <div className="tools__frame">
-              <img src={skill.icon} alt={skill.name} className="tools__icon" />
-              <span className="tools__name">{skill.name}</span>
-            </div>
-          </li>
-        ))}
-      </ul>
+      {/* --- CONTENIDO PRINCIPAL --- */}
+      <div className="tools__content tools__content--katanas">
+        <h2 className="tools__title">Herramientas</h2>
+
+        <ul className="tools__list">
+          {skillsData.map((skill, index) => (
+            <li
+              key={skill.id}
+              className="tools__item"
+              style={{ animationDelay: `${index * 0.2}s` }}
+            >
+              {/* CONTENEDOR CUADRO */}
+              <div className="tools__frame">
+                <img
+                  src={skill.icon}
+                  alt={skill.name}
+                  className="tools__icon"
+                />
+                <span className="tools__name">{skill.name}</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {/* --- SUELO --- */}
+      <div className="tools__wall"></div>
+      <div className="tools__floor"></div>
     </section>
   );
 }
